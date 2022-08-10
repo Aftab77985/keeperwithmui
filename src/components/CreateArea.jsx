b187d169cbc88153a987b7a30c37cab9f5c761e4
Aftab Ahmed <aaftab_2013@yahoo.com>
2022-08-10 15:46:22 +0500

    initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab } from '@mui/material';
+import Zoom from '@mui/material/Zoom';
+
+function CreateArea(props) {
+  const [isEffect , setEffect ] = useState(false);
+  function handleEffect (){
+    setEffect(true)
+
+  }
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isEffect ? <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        /> : null}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={handleEffect}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isEffect ? "3" : "1"}
+        />
+        <Zoom in={isEffect}>
+        <Fab onClick={submitNote}>
+        <AddIcon />
+        </Fab>
+        </Zoom>
+        
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
